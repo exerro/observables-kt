9701529b29e0c32ec541e27b021acf18bf75adde
exerro <benedict.allen2514@gmail.com>
2019-09-05 19:53:58 +0100

    Update README and add Observable

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # observables-kt
-Implements observable values and signals for Kotlin
+
+Implement observable values and signals for Kotlin.
+
+## Signals
+
+A signal is similar to an event.
+
+One may connect functions (callbacks) to the signal, and they will be invoked
+when the signal is "emitted".
+
+Some variants of signals take parameters as well, which are given to emit and
+passed to the callbacks connected when invoked.
+
+Signals are all thread-safe.
+
+## Observables
+
+An `Observable<T>` is a wrapper around a value of type `T`  which has a
+single `changed` signal that is emitted with the new value of the object
+when changed.
+
+An observable is itself a `Connectable` (acting like a signal), where
+connecting directly to the observable will also invoke the callback
+immediately with the current value of the observable.
---
+++ src/Observable.kt
@@ -0,0 +1,45 @@
+
+/** An observed value which emits a signal when changed. */
+class Observable<T>(value: T): Connectable<(T) -> Unit> {
+    /** Signal emitted when the value is changed, with the new value as a
+     *  parameter */
+    val changed = Signal<T>()
+
+    /** The current value of the object. */
+    var value: T = value
+        private set
+
+    /** Set the new value of the object, emitting the changed signal. */
+    fun value(value: T): T {
+        var hasChanged = false
+
+        synchronized(lock) {
+            if (this.value != value) {
+                this.value = value
+                hasChanged = true
+            }
+        }
+
+        if (hasChanged) changed.emit(value)
+
+        return value
+    }
+
+    /** Set the new value of the object, emitting the changed signal. */
+    operator fun invoke(value: T) = value(value)
+
+    /** Connect a callback to the changed signal, and call it once immediately
+     *  with the current value of the object. */
+    override fun connect(fn: (T) -> Unit)
+            = changed.connect(fn).also { fn(value) }
+
+    /** Disconnect a callback from the changed signal. */
+    override fun disconnect(fn: (T) -> Unit)
+            = changed.disconnect(fn)
+
+    /** Disconnect all callbacks from the changed signal. */
+    override fun disconnectAll()
+            = changed.disconnectAll()
+
+    private val lock = object {}
+}
